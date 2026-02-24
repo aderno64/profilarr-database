5a6b89bc7537f306338e369eb25153e143553aca
SweatyEggs69 <kyle@mcclain.tech>
2026-02-24 22:06:45 +0000

    Added SM737 to Banned per Dictionarry

---
+++ ops/13.added-sm737-to-banned-per-dictionarry.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Added SM737 to Banned per Dictionarry
+-- @exportedAt: 2026-02-24T22:06:44.323Z
+-- @opIds: 331
+
+-- --- BEGIN op 331 ( update regular_expression "Dictionarry Banned Groups" )
+update "regular_expressions" set "pattern" = '(?<=^|[\s.-])(4K4U|AOC|AROMA|BeyondHD|BiTOR|BLASPHEMY|BOLS|BTM|CLASSiCALHD|CREATiVE24|d3g|DepraveD|DeViSiVE|DRX|E|FGT|Flights|HDS|iVy|jennaortegaUHD|KC|LAMA|MeGusta|MgB|NAHOM|NhaNc3|NoGroup|OEPlus|PSA|RARGB|SasukeducK|SHD|ShieldBearer|SM737|STUTTERSHIT|tarunk9c|TEKNO3D|Telly|TvR|VD0N|VECTOR|VisionXpert|x0r|YIFY|YTS)\b' where "name" = 'Dictionarry Banned Groups' and "pattern" = '(?<=^|[\s.-])(4K4U|AOC|AROMA|BeyondHD|BiTOR|BLASPHEMY|BOLS|BTM|CLASSiCALHD|CREATiVE24|d3g|DepraveD|DeViSiVE|DRX|E|FGT|Flights|HDS|iVy|jennaortegaUHD|KC|LAMA|MeGusta|MgB|NAHOM|NhaNc3|NoGroup|OEPlus|PSA|RARGB|SasukeducK|SHD|ShieldBearer|STUTTERSHIT|tarunk9c|TEKNO3D|Telly|TvR|VD0N|VECTOR|VisionXpert|x0r|YIFY|YTS)\b';
+-- --- END op 331
