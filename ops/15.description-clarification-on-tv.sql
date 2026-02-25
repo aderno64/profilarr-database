afd970c87d45cb7f5f1171f4643a7ae1c1101725
SweatyEggs69 <kyle@mcclain.tech>
2026-02-25 21:31:50 +0000

    Description clarification on TV

---
+++ ops/15.description-clarification-on-tv.sql
@@ -0,0 +1,43 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Description clarification on TV
+-- @exportedAt: 2026-02-25T21:31:49.627Z
+-- @opIds: 337, 338, 339, 340
+
+-- --- BEGIN op 337 ( update quality_profile "TV 1080p" )
+update "quality_profiles" set "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.
+- x265/HEVC is allowed on this profile, so some transcoding is possible if your client does not support x265/HEVC.' where "name" = 'TV 1080p' and "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.';
+-- --- END op 337
+
+-- --- BEGIN op 338 ( update quality_profile "TV 2160p" )
+update "quality_profiles" set "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.
+- x265/HEVC is allowed on this profile, so some transcoding is possible if your client does not support x265/HEVC' where "name" = 'TV 2160p' and "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.';
+-- --- END op 338
+
+-- --- BEGIN op 339 ( update quality_profile "TV 1080p" )
+update "quality_profiles" set "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.
+- x265/HEVC is **allowed** on this profile, so some transcoding is possible if your client **does not** support x265/HEVC.' where "name" = 'TV 1080p' and "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.
+- x265/HEVC is allowed on this profile, so some transcoding is possible if your client does not support x265/HEVC.';
+-- --- END op 339
+
+-- --- BEGIN op 340 ( update quality_profile "TV 2160p" )
+update "quality_profiles" set "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.
+- x265/HEVC is **allowed** on this profile, so some transcoding is possible if your client **does not** support x265/HEVC' where "name" = 'TV 2160p' and "description" = 'Based on the TRaSH Guides WEB-1080p (Alternative) profile, focusing on balanced quality/file size.
+- This profile is recommended for a set-it-and-forget-it setup.
+- Ensure you add a **[delay profile](https://wiki.servarr.com/sonarr/settings#:~:text=%C2%B6-,Delay%20Profiles,-Delay%20profiles%20allow)** to your setup. I prefer 60 minutes, but adjust this as needed.
+- x265/HEVC is allowed on this profile, so some transcoding is possible if your client does not support x265/HEVC';
+-- --- END op 340
