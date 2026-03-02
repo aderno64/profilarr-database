9f4197c1c14ccbf0d128f6da12009d1fa4baa433
SweatyEggs69 <kyle@mcclain.tech>
2026-03-02 18:11:33 +0000

    More more more cleanup

---
+++ ops/41.more-more-more-cleanup.sql
@@ -0,0 +1,133 @@
+-- @operation: export
+-- @entity: batch
+-- @name: More more more cleanup
+-- @exportedAt: 2026-03-02T18:11:29.096Z
+-- @opIds: 1105, 1106, 1107, 1108, 1109, 1110, 1111, 1112, 1113, 1114, 1115, 1116, 1117, 1118, 1119, 1120, 1121, 1122, 1123, 1124, 1125, 1126, 1127, 1128, 1129, 1130, 1131, 1132, 1133, 1134, 1135, 1136
+
+-- --- BEGIN op 1105 ( update regular_expression "Dictionarry WEB Tier 01" )
+update "regular_expressions" set "pattern" = '^(FLUX|NTb|TheFarm)$' where "name" = 'Dictionarry WEB Tier 01' and "pattern" = '\b(FLUX|NTb|TheFarm)\b';
+-- --- END op 1105
+
+-- --- BEGIN op 1106 ( update regular_expression "Dictionarry WEB Tier 02" )
+update "regular_expressions" set "pattern" = '^(BYNDR|CMRG|HONE|Kitsune|playWEB|TEPES)$' where "name" = 'Dictionarry WEB Tier 02' and "pattern" = '\b(BYNDR|CMRG|HONE|Kitsune|playWEB|TEPES)\b';
+-- --- END op 1106
+
+-- --- BEGIN op 1107 ( update regular_expression "Dictionarry WEB Tier 03" )
+update "regular_expressions" set "pattern" = '^(ABM|AJP69|BLUTONiUM|BTW|CasStudio|Cinefeel|CRFW|CtrlHD|HHWEB|KHN|KiNGS|LAZY|monkee|MZABI|NOSiViD|NPMS|NTG|QOQ|RAWR|SA89|SbR|SMURF|TeamSyndicate|TOMMY|TVSmash|WELP|XEBEC|ZoroSenpai)$' where "name" = 'Dictionarry WEB Tier 03' and "pattern" = '\b(ABM|AJP69|BLUTONiUM|BTW|CasStudio|Cinefeel|CRFW|CtrlHD|HHWEB|KHN|KiNGS|LAZY|monkee|MZABI|NOSiViD|NPMS|NTG|QOQ|RAWR|SA89|SbR|SMURF|TeamSyndicate|TOMMY|TVSmash|WELP|XEBEC|ZoroSenpai)\b';
+-- --- END op 1107
+
+-- --- BEGIN op 1108 ( update regular_expression "Dictionarry WEB Tier 04" )
+update "regular_expressions" set "pattern" = '^(APEX|BLOOM|BTN|dB|PTP|RTN|SiC|SPiRiT|ViSUM)$' where "name" = 'Dictionarry WEB Tier 04' and "pattern" = '\b(APEX|BLOOM|BTN|dB|PTP|RTN|SiC|SPiRiT|ViSUM)\b';
+-- --- END op 1108
+
+-- --- BEGIN op 1109 ( update regular_expression "Dictionarry WEB Tier 05" )
+update "regular_expressions" set "pattern" = '^(CRUD|GNOME|NINJACENTRAL|PHOENiX|ROCCaT|SiGMA|SwAgLaNdEr)$' where "name" = 'Dictionarry WEB Tier 05' and "pattern" = '\b(CRUD|GNOME|NINJACENTRAL|PHOENiX|ROCCaT|SiGMA|SwAgLaNdEr)\b';
+-- --- END op 1109
+
+-- --- BEGIN op 1110 ( update regular_expression "Dumpstarr WEB Boost" )
+update "regular_expressions" set "pattern" = '^(Kitsune)$' where "name" = 'Dumpstarr WEB Boost' and "pattern" = '\b(Kitsune)\b';
+-- --- END op 1110
+
+-- --- BEGIN op 1111 ( update regular_expression "Dumpstarr WEB Tier 02" )
+update "regular_expressions" set "pattern" = '^(DiscoD|Yogi)$' where "name" = 'Dumpstarr WEB Tier 02' and "pattern" = '\b(DiscoD|Yogi)\b';
+-- --- END op 1111
+
+-- --- BEGIN op 1112 ( update regular_expression "TRaSH WEB Tier 01" )
+update "regular_expressions" set "pattern" = '^(ABBIE|AJP69|APEX|BLUTONiUM|BYNDR|CMRG|CRFW|CRUD|FLUX|GNOME|HONE|KiNGS|Kitsune|NOSiViD|NTb|NTG|RAWR|SiC|TEPES|TheFarm|ZoroSenpai)$' where "name" = 'TRaSH WEB Tier 01' and "pattern" = '\b(ABBIE|AJP69|APEX|BLUTONiUM|BYNDR|CMRG|CRFW|CRUD|FLUX|GNOME|HONE|KiNGS|Kitsune|NOSiViD|NTb|NTG|RAWR|SiC|TEPES|TheFarm|ZoroSenpai)\b';
+-- --- END op 1112
+
+-- --- BEGIN op 1113 ( update regular_expression "TRaSH WEB Tier 02" )
+update "regular_expressions" set "pattern" = '^(dB|Flights|MiU|monkee|MZABI|PHOENiX|playWEB|RDNYB|SbR|SMURF|TOMMY|XEBEC)$' where "name" = 'TRaSH WEB Tier 02' and "pattern" = '\b(dB|Flights|MiU|monkee|MZABI|PHOENiX|playWEB|RDNYB|SbR|SMURF|TOMMY|XEBEC)\b';
+-- --- END op 1113
+
+-- --- BEGIN op 1114 ( update regular_expression "TRaSH WEB Tier 03" )
+update "regular_expressions" set "pattern" = '^(BLOOM|GNOMiSSiON|HHWEB|NINJACENTRAL|NPMS|QOQ|ROCCaT|SiGMA|SLiGNOME|SwAgLaNdEr)$' where "name" = 'TRaSH WEB Tier 03' and "pattern" = '\b(BLOOM|GNOMiSSiON|HHWEB|NINJACENTRAL|NPMS|QOQ|ROCCaT|SiGMA|SLiGNOME|SwAgLaNdEr)\b';
+-- --- END op 1114
+
+-- --- BEGIN op 1115 ( update regular_expression "Dictionarry Remux Tier 01" )
+update "regular_expressions" set "pattern" = '^(3L|BiZKiT|BLURANiUM|CiNEPHiLES|WiLDCAT)$' where "name" = 'Dictionarry Remux Tier 01' and "pattern" = '\b(3L|BiZKiT|BLURANiUM|CiNEPHiLES|WiLDCAT)\b';
+-- --- END op 1115
+
+-- --- BEGIN op 1116 ( update regular_expression "Dictionarry Remux Tier 02" )
+update "regular_expressions" set "pattern" = '^(BMF|FraMeSToR|LM|PiRAMiDHEAD|PmP|PP|ZoroSenpai|ZQ)$' where "name" = 'Dictionarry Remux Tier 02' and "pattern" = '\b(BMF|FraMeSToR|LM|PiRAMiDHEAD|PmP|PP|ZoroSenpai|ZQ)\b';
+-- --- END op 1116
+
+-- --- BEGIN op 1117 ( update regular_expression "Dictionarry Remux Tier 03" )
+update "regular_expressions" set "pattern" = '^(ATELiER|decibeL|EPSiLON|HiFi|NCmt|SiCFoI|TEPES)$' where "name" = 'Dictionarry Remux Tier 03' and "pattern" = '\b(ATELiER|decibeL|EPSiLON|HiFi|NCmt|SiCFoI|TEPES)\b';
+-- --- END op 1117
+
+-- --- BEGIN op 1118 ( update regular_expression "Dictionarry Remux Tier 04" )
+update "regular_expressions" set "pattern" = '^(BTN|iFT|KRaLiMaRKo|playBD|PTP|TRiToN)$' where "name" = 'Dictionarry Remux Tier 04' and "pattern" = '\b(BTN|iFT|KRaLiMaRKo|playBD|PTP|TRiToN)\b';
+-- --- END op 1118
+
+-- --- BEGIN op 1119 ( update regular_expression "TRaSH Remux Tier 01 - Radarr" )
+update "regular_expressions" set "pattern" = '^(3L|BiZKiT|BLURANiUM|BMF|CiNEPHiLES|FraMeSToR|PiRAMiDHEAD|PmP|WiLDCAT|ZQ)$' where "name" = 'TRaSH Remux Tier 01 - Radarr' and "pattern" = '\b(3L|BiZKiT|BLURANiUM|BMF|CiNEPHiLES|FraMeSToR|PiRAMiDHEAD|PmP|WiLDCAT|ZQ)\b';
+-- --- END op 1119
+
+-- --- BEGIN op 1120 ( update regular_expression "TRaSH Remux Tier 01 - Sonarr" )
+update "regular_expressions" set "pattern" = '^(BLURANiUM|BMF|FraMeSToR|PmP)$' where "name" = 'TRaSH Remux Tier 01 - Sonarr' and "pattern" = '\b(BLURANiUM|BMF|FraMeSToR|PmP)\b';
+-- --- END op 1120
+
+-- --- BEGIN op 1121 ( update regular_expression "TRaSH Remux Tier 02 - Radarr" )
+update "regular_expressions" set "pattern" = '^(NCmt|playBD|SiCFoI|SURFINBIRD|TEPES)$' where "name" = 'TRaSH Remux Tier 02 - Radarr' and "pattern" = '\b(NCmt|playBD|SiCFoI|SURFINBIRD|TEPES)\b';
+-- --- END op 1121
+
+-- --- BEGIN op 1122 ( update regular_expression "TRaSH Remux Tier 02 - Sonarr" )
+update "regular_expressions" set "pattern" = '^(decibeL|EPSiLON|HiFi|KRaLiMaRKo|playBD|PTer|SiCFoI|TRiToN)$' where "name" = 'TRaSH Remux Tier 02 - Sonarr' and "pattern" = '\b(decibeL|EPSiLON|HiFi|KRaLiMaRKo|playBD|PTer|SiCFoI|TRiToN)\b';
+-- --- END op 1122
+
+-- --- BEGIN op 1123 ( update regular_expression "TRaSH Remux Tier 03 - Radarr" )
+update "regular_expressions" set "pattern" = '^(ATELiER|decibeL|EPSiLON|HiFi|iFT|KRaLiMaRKo|NTb|PTP|SumVision|TOA|TRiToN)$' where "name" = 'TRaSH Remux Tier 03 - Radarr' and "pattern" = '\b(ATELiER|decibeL|EPSiLON|HiFi|iFT|KRaLiMaRKo|NTb|PTP|SumVision|TOA|TRiToN)\b';
+-- --- END op 1123
+
+-- --- BEGIN op 1124 ( update regular_expression "TRaSH HD Bluray Tier 01" )
+update "regular_expressions" set "pattern" = '^(BBQ|BMF|c0kE|Chotab|CRiSC|CtrlHD|D-Z0N3|Dariush|decibeL|DON|EbP|EDPH|Geek|LolHD|NCmt|PTer|TayTO|TDD|TnP|VietHD|ZQ|ZoroSenpai)$' where "name" = 'TRaSH HD Bluray Tier 01' and "pattern" = '\b(BBQ|BMF|c0kE|Chotab|CRiSC|CtrlHD|D-Z0N3|Dariush|decibeL|DON|EbP|EDPH|Geek|LolHD|NCmt|PTer|TayTO|TDD|TnP|VietHD|ZQ|ZoroSenpai)\b';
+-- --- END op 1124
+
+-- --- BEGIN op 1125 ( update regular_expression "TRaSH HD Bluray Tier 02" )
+update "regular_expressions" set "pattern" = '^(EA|HiDt|HiSD|iFT|NTb|QOQ|SA89|sbR)$' where "name" = 'TRaSH HD Bluray Tier 02' and "pattern" = '\b(EA|HiDt|HiSD|iFT|NTb|QOQ|SA89|sbR)\b';
+-- --- END op 1125
+
+-- --- BEGIN op 1126 ( update regular_expression "TRaSH HD Bluray Tier 03" )
+update "regular_expressions" set "pattern" = '^(ATELiER|BHDStudio|hallowed|HiFi|HONE|LoRD|playHD|SPHD|W4NK3R)$' where "name" = 'TRaSH HD Bluray Tier 03' and "pattern" = '\b(ATELiER|BHDStudio|hallowed|HiFi|HONE|LoRD|playHD|SPHD|W4NK3R)\b';
+-- --- END op 1126
+
+-- --- BEGIN op 1127 ( update regular_expression "TRaSH UHD Bluray Tier 01" )
+update "regular_expressions" set "pattern" = '^(CtrlHD|MainFrame|DON|W4NK3R)$' where "name" = 'TRaSH UHD Bluray Tier 01' and "pattern" = '\b(CtrlHD|MainFrame|DON|W4NK3R)\b';
+-- --- END op 1127
+
+-- --- BEGIN op 1128 ( update regular_expression "TRaSH UHD Bluray Tier 02" )
+update "regular_expressions" set "pattern" = '^(HQMUX)$' where "name" = 'TRaSH UHD Bluray Tier 02' and "pattern" = '\b(HQMUX)\b';
+-- --- END op 1128
+
+-- --- BEGIN op 1129 ( update regular_expression "TRaSH UHD Bluray Tier 03" )
+update "regular_expressions" set "pattern" = '^(BHDStudio|hallowed|HONE|PTer|SPHD|WEBDV)$' where "name" = 'TRaSH UHD Bluray Tier 03' and "pattern" = '\b(BHDStudio|hallowed|HONE|PTer|SPHD|WEBDV)\b';
+-- --- END op 1129
+
+-- --- BEGIN op 1130 ( update regular_expression "Dumpstarr 1080p Tier 03" )
+update "regular_expressions" set "pattern" = '^(DarQ|DiscoD|HONE|Yogi)$' where "name" = 'Dumpstarr 1080p Tier 03' and "pattern" = '\b(DarQ|DiscoD|HONE|Yogi)\b';
+-- --- END op 1130
+
+-- --- BEGIN op 1131 ( update regular_expression "Dumpstarr 2160p Tier 03" )
+update "regular_expressions" set "pattern" = '^(DarQ|DiscoD|HONE|Yogi)$' where "name" = 'Dumpstarr 2160p Tier 03' and "pattern" = '\b(DarQ|DiscoD|HONE|Yogi)\b';
+-- --- END op 1131
+
+-- --- BEGIN op 1132 ( update regular_expression "Dumpstarr Banned Groups" )
+update "regular_expressions" set "pattern" = '^(DEiMOS|MIRCrew|MuTT|TiZU|Tofu4U)$' where "name" = 'Dumpstarr Banned Groups' and "pattern" = '\b(DEiMOS|MIRCrew|MuTT|TiZU|Tofu4U)\b';
+-- --- END op 1132
+
+-- --- BEGIN op 1133 ( update regular_expression "Dumpstarr Banned Groups (Title)" )
+update "regular_expressions" set "pattern" = '^(E|Tofu4U)$' where "name" = 'Dumpstarr Banned Groups (Title)' and "pattern" = '\b(E|Tofu4U)\b';
+-- --- END op 1133
+
+-- --- BEGIN op 1134 ( update regular_expression "Dumpstarr Baseline Groups" )
+update "regular_expressions" set "pattern" = '^(alfaHD|afm72|Bandi|Bluespots|Celdra|DUSKLiGHT|FreetheFish|Garshasp|Ghost|Ime|Kappa|Langbard|LION|Panda|MONOLITH|Natty|PiRaTeS|QxR|r00t|RCVR|RZeroX|SAMPA|Silence|SuccessfulCrab|t3nzin|Tigole|Vyndros)$' where "name" = 'Dumpstarr Baseline Groups' and "pattern" = '\b(alfaHD|afm72|Bandi|Bluespots|Celdra|DUSKLiGHT|FreetheFish|Garshasp|Ghost|Ime|Kappa|Langbard|LION|Panda|MONOLITH|Natty|PiRaTeS|QxR|r00t|RCVR|RZeroX|SAMPA|Silence|SuccessfulCrab|t3nzin|Tigole|Vyndros)\b';
+-- --- END op 1134
+
+-- --- BEGIN op 1135 ( update regular_expression "Dumpstarr SIDCA Fallback Groups" )
+update "regular_expressions" set "pattern" = '^(ASTRiD|CBFM|CtrlHD|DARKFLiX|DEFLATE|EDPH|HANDJOB|HODL|iVy|KONTRAST|MacP|PLUTONiUM|PHOCiS|RARBG|ViETNAM|YAWNiX|YIFY|YTS)$' where "name" = 'Dumpstarr SIDCA Fallback Groups' and "pattern" = '\b(ASTRiD|CBFM|CtrlHD|DARKFLiX|DEFLATE|EDPH|HANDJOB|HODL|iVy|KONTRAST|MacP|PLUTONiUM|PHOCiS|RARBG|ViETNAM|YAWNiX|YIFY|YTS)\b';
+-- --- END op 1135
+
+-- --- BEGIN op 1136 ( update regular_expression "Dumpstarr SIDCA Groups" )
+update "regular_expressions" set "pattern" = '^(ELiTE|MeGusta|PSA)$' where "name" = 'Dumpstarr SIDCA Groups' and "pattern" = '\b(ELiTE|MeGusta|PSA)\b';
+-- --- END op 1136
