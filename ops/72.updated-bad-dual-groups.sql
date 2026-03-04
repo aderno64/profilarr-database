eca629371fa91ea302d89d7cf55bfb0fb93e7773
SweatyEggs69 <kyle@mcclain.tech>
2026-03-04 01:23:50 +0000

    Updated Bad Dual Groups

---
+++ ops/72.updated-bad-dual-groups.sql
@@ -0,0 +1,13 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Updated Bad Dual Groups
+-- @exportedAt: 2026-03-04T01:23:49.073Z
+-- @opIds: 1928, 1929
+
+-- --- BEGIN op 1928 ( update regular_expression "Bad Dual Groups" )
+update "regular_expressions" set "pattern" = '\b(alfaHD.*|BAT|BiOMA|BlackBit|BNd|Cory|EXTREME|FF|FOXX|G4RiS|GUEIRA|LCD|MGE\b.*|N3G4N|ONLYMOViE|PD|PTHome|RiPER|RK|SiGLA|Tars|TM\b|TURG|tokar86a|TvR|vnlls|WTV|Yatogam1|YusukeFLA|ZigZag|ZNM)\b' where "name" = 'Bad Dual Groups' and "pattern" = '\b(alfaHD.*|BAT|BiOMA|BlackBit|BNd|Cory|EXTREME|FF|FOXX|G4RiS|GUEIRA|LCD|N3G4N|ONLYMOViE|PD|PTHome|RiPER|RK|SiGLA|Tars|TURG|tokar86a|TvR|vnlls|WTV|Yatogam1|YusukeFLA|ZigZag|ZNM)\b';
+-- --- END op 1928
+
+-- --- BEGIN op 1929 ( update regular_expression "Bad Dual Groups" )
+update "regular_expressions" set "pattern" = '\b(alfaHD.*|BAT|BiOMA|BlackBit|BNd|C\.A\.A|Cory|EXTREME|FF|FOXX|G4RiS|GUEIRA|LCD|MGE\b.*|N3G4N|ONLYMOViE|PD|PTHome|RiPER|RK|SiGLA|Tars|TM\b|TURG|tokar86a|TvR|vnlls|WTV|Yatogam1|YusukeFLA|ZigZag|ZNM)\b' where "name" = 'Bad Dual Groups' and "pattern" = '\b(alfaHD.*|BAT|BiOMA|BlackBit|BNd|Cory|EXTREME|FF|FOXX|G4RiS|GUEIRA|LCD|MGE\b.*|N3G4N|ONLYMOViE|PD|PTHome|RiPER|RK|SiGLA|Tars|TM\b|TURG|tokar86a|TvR|vnlls|WTV|Yatogam1|YusukeFLA|ZigZag|ZNM)\b';
+-- --- END op 1929
