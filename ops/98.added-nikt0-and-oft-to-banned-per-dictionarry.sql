bc8720082aff9d3a14b27bc09328235e167210e7
SweatyEggs69 <kyle@mcclain.tech>
2026-03-05 19:21:58 +0000

    Added nikt0 and OFT to Banned per Dictionarry

---
+++ ops/98.added-nikt0-and-oft-to-banned-per-dictionarry.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: Added nikt0 and OFT to Banned per Dictionarry
+-- @exportedAt: 2026-03-05T19:21:57.411Z
+-- @opIds: 2479
+
+-- --- BEGIN op 2479 ( update regular_expression "Dictionarry Banned Groups" )
+update "regular_expressions" set "pattern" = '\b(4K4U|AOC|AROMA|BeyondHD|BiTOR|BLASPHEMY|BOLS|BTM|CLASSiCALHD|CREATiVE24|d3g|DepraveD|DeViSiVE|DRX|E|FGT|Flights|HDS|iVy|jennaortegaUHD|KC|LAMA|MeGusta|MgB|NAHOM|NhaNc3|nikt0|NoGroup|OEPlus|OFT|PSA|RARGB|SasukeducK|SHD|ShieldBearer|SM737|STUTTERSHIT|tarunk9c|TEKNO3D|Telly|TvR|VD0N|VECTOR|VisionXpert|x0r|YIFY|YTS)\b' where "name" = 'Dictionarry Banned Groups' and "pattern" = '\b(4K4U|AOC|AROMA|BeyondHD|BiTOR|BLASPHEMY|BOLS|BTM|CLASSiCALHD|CREATiVE24|d3g|DepraveD|DeViSiVE|DRX|E|FGT|Flights|HDS|iVy|jennaortegaUHD|KC|LAMA|MeGusta|MgB|NAHOM|NhaNc3|NoGroup|OEPlus|PSA|RARGB|SasukeducK|SHD|ShieldBearer|SM737|STUTTERSHIT|tarunk9c|TEKNO3D|Telly|TvR|VD0N|VECTOR|VisionXpert|x0r|YIFY|YTS)\b';
+-- --- END op 2479
